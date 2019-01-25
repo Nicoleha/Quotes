34fb2aa631c021586c922107293d14567f02da45
Nicole Hategekimana <nicoleha127@gmail.com>
2019-01-25 12:44:06 +0200

    local hosting page initiated

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -7,14 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesComponent implements OnInit {
 
-  quotes=[
-    new quote('Life is just a chance to grow a soul.',' A. Powell Davies',20,0),
-          new quote('Only a life lived for others is a life worthwhile.',' Albert Einstein',19,1 ),
-          new quote('Expect nothing, live frugally on surprise.','Alice Walker',14,4),
-          new quote('Because I have loved life, I shall have no sorrow to die.',' Amelia Burr',12,3),
-          new quote('People living deeply have no fear of death.','Anais Nin',12,5 ),
-        
-  ]
+  quotes='one'
 
   constructor() { }
 
