9e291f8eb9537e04ff890bd3e571730dc91d8ac3
Nicole Hategekimana <nicoleha127@gmail.com>
2019-01-25 12:26:53 +0200

    Quotes app initialised

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Quotes';
+  
+  // quotes=[
+  //   new quote('Life is just a chance to grow a soul.',' A. Powell Davies',20,0),
+  //         new quote('Only a life lived for others is a life worthwhile.',' Albert Einstein',19,1 ),
+  //         new quote('Expect nothing, live frugally on surprise.','Alice Walker',14,4),
+  //         new quote('Because I have loved life, I shall have no sorrow to die.',' Amelia Burr',12,3),
+  //         new quote('People living deeply have no fear of death.','Anais Nin',12,5 ),
+        
+  // ]
 }
