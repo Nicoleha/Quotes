9970d823c549ec3e0c1fab840a3e5775484a42af
Nicole Hategekimana <nicoleha127@gmail.com>
2019-01-25 13:12:29 +0200

    quote components editted

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,7 @@
 export class Quotes {
+    public showDescription:boolean
+    constructor(public name:string,public author:string,public upvote:number,public downvote:number){
+        this.showDescription=false
+
+    }
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Quotes} from '../quotes';
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.component.html',
@@ -7,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesComponent implements OnInit {
 
-  quotes='one'
+  quotes=[
+    new Quotes('Life is just a chance to grow a soul.',' A. Powell Davies',20,0),
+          new Quotes('Only a life lived for others is a life worthwhile.',' Albert Einstein',19,1 ),
+          new Quotes('Expect nothing, live frugally on surprise.','Alice Walker',14,4),
+          new Quotes('Because I have loved life, I shall have no sorrow to die.',' Amelia Burr',12,3),
+          new Quotes('People living deeply have no fear of death.','Anais Nin',12,5 ),
+        
+  ]
 
   constructor() { }
 
