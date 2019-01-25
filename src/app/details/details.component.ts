651b03ed6a1f3f07e7eb236c89b49b7d2489e87a
Nicole Hategekimana <nicoleha127@gmail.com>
2019-01-25 16:51:55 +0200

    component details done

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Quotes} from '../quotes'
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent implements OnInit {
- 
+
   constructor() { }
 
   ngOnInit() {
